a4feaccc85f4ad283a6477faada03c083703a1d6
Piotr Olejniczak <piotr.olejniczak@makimo.pl>
2019-06-12 11:12:05 +0200

    Code beautification MATHNET-5

--- src/Admin.js
+++ src/Admin.js
@@ -1369,7 +1369,7 @@ class Admin {
             this.server_error(data.message);
         });
 
-        socket.on('ping_mathnet-response', (time) => {
+        socket.on('ping-mathnet-response', (time) => {
             this.ping_response(time);
         });
 
@@ -1465,7 +1465,7 @@ class Admin {
 
     // This function takes a time and pings it
     ping(time) {
-        this.socket.emit('ping_mathnet', time);
+        this.socket.emit('ping-mathnet', time);
     }
 
 
--- src/Student.js
+++ src/Student.js
@@ -81,7 +81,7 @@ class StudentController {
             this.server_error(data.message);
         });
 
-        socket.on('ping_mathnet-response', time => {
+        socket.on('ping-mathnet-response', time => {
             this.ping_response(time);
         });
 
@@ -455,7 +455,7 @@ class StudentController {
 
     // This function takes a time and pings it
     ping(time) {
-        this.socket.emit('ping_mathnet', time);
+        this.socket.emit('ping-mathnet', time);
     };
 
     //This function takes a username and class_id from the user
